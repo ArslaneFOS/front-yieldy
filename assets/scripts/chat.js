3e70ad5790cfad013fe1d9f52586785459108cda
ArslaneFOS <arslane02@gmail.com>
2022-06-11 12:54:00 +0100

    fix + 404 + construction page

--- assets/scripts/chat.js
+++ assets/scripts/chat.js
@@ -1,27 +1,16 @@
-const openChat = () => {
-    var chat = document.getElementById('chat');
-
-    chat.classList.remove('invisible');
-    document.body.classList.add('stop-scroll')
-}
-
-const closeChat = () => {
-    var chat = document.getElementById('chat');
-
-    chat.classList.add('invisible')
-    document.body.classList.remove('stop-scroll')
-}
-
 const toggleChat = () => {
     var chat = document.getElementById('chat');
 
-    if (chat.classList.contains('invisible')) {
+    chat.classList.toggle("invisible");
+    document.body.classList.toggle("stop-scroll");
+
+    /*if (chat.classList.contains('invisible')) {
         openChat();
     } else {
         closeChat();
-    }
+    }*/
 }
 
 document.getElementById('toggle-chat').onclick = () => toggleChat();
 
-document.getElementById('close-chat').onclick = () => closeChat();
+document.getElementById('close-chat').onclick = () => toggleChat();
--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -2,56 +2,27 @@ var question_headers = document.querySelectorAll(".question-header");
 question_headers.forEach(question_header => {
     question_header.onclick = () => {
         var attachedQuestionBody = question_header.parentElement.querySelector(".question-body");
-        if (attachedQuestionBody.className == "question-body qb-inactive") {
-            attachedQuestionBody.className = "question-body qb-active";
-            question_header.querySelector(".dropdown-btn").className = "dropdown-btn dd-btn-active"
-        } else {
-            attachedQuestionBody.className = "question-body qb-inactive";
-            question_header.querySelector(".dropdown-btn").className = "dropdown-btn"
-        }
 
-
+        attachedQuestionBody.classList.toggle("qb-inactive");
+        attachedQuestionBody.classList.toggle("qb-active");
+        question_header.querySelector(".dropdown-btn").classList.toggle("dd-btn-active");
     }
 });
 
 var hamburgerBtn = document.querySelector(".hamburger");
 hamburgerBtn.onclick = () => {
     var mobileNavBar = document.querySelector(".nav-mobile");
-    console.log(mobileNavBar);
 
-    if (mobileNavBar.className == "nav-mobile") {
-        mobileNavBar.classList.add("nvm-active");
-        document.body.classList.add("stop-scroll");
-    } else {
-        mobileNavBar.classList.remove("nvm-active")
-        document.body.classList.remove("stop-scroll");
-    }
-}
-
-const openDlOl = () => {
-    var downloadOverlay = document.getElementById('download-overlay');
-
-    downloadOverlay.classList.remove('invisible');
-    document.body.classList.add('stop-scroll')
-    document.querySelector("main").style.filter = "contrast(50%) brightness(110%)";
-}
-
-const closeDlOl = () => {
-    var downloadOverlay = document.getElementById('download-overlay');
-
-    downloadOverlay.classList.add('invisible')
-    document.body.classList.remove('stop-scroll')
-    document.querySelector("main").style.filter = "";
+    mobileNavBar.classList.toggle("nvm-active");
+    document.body.classList.toggle("stop-scroll");
 }
 
 const toggleDlOl = () => {
     var downloadOverlay = document.getElementById('download-overlay');
 
-    if (downloadOverlay.classList.contains('invisible')) {
-        openDlOl();
-    } else {
-        closeDlOl();
-    }
+    downloadOverlay.classList.toggle("invisible");
+    document.body.classList.toggle("stop-scroll");
+    document.querySelector("main").classList.toggle("overlayed");
 }
 
 var downloadBtns = document.querySelectorAll("header a.btn-wbg");
@@ -67,12 +38,11 @@ document.body.onload = () => {
     }    
     else {
         downloadBtns.forEach(btn => {
-            console.log(btn);
             btn.onclick = () => {
                 toggleDlOl();
             }
 
-            document.querySelector("#close-download").onclick = () => closeDlOl();
         });
+        document.querySelector("#close-download").onclick = () => toggleDlOl();
     }
 }
